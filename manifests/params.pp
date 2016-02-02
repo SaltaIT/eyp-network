902cacfd1592630a9747953c07273b610f6d8d4b
Jordi Prats <jprats@systemadmin.es>
2016-02-02 18:31:35 +0100

    lint

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 class network::params {
 
-	case $::osfamily
-	{
+  case $::osfamily
+  {
     'redhat':
     {
       $service_restart_exec='service network restart'
@@ -13,25 +13,25 @@ class network::params {
         default: { fail("Unsupported RHEL/CentOS version! - $::operatingsystemrelease")  }
       }
     }
-		'Debian':
-		{
+    'Debian':
+    {
       $service_restart_exec='/sbin/ifdown -a && /sbin/ifup -a'
-			case $::operatingsystem
-			{
-				'Ubuntu':
-				{
-					case $::operatingsystemrelease
-					{
-						/^14.*$/:
-						{
-						}
-						default: { fail("Unsupported Ubuntu version! - $::operatingsystemrelease")  }
-					}
-				}
-				'Debian': { fail('Unsupported')  }
-				default: { fail('Unsupported Debian flavour!')  }
-			}
-		}
-		default: { fail('Unsupported OS!')  }
-	}
+      case $::operatingsystem
+      {
+        'Ubuntu':
+        {
+          case $::operatingsystemrelease
+          {
+            /^14.*$/:
+            {
+            }
+            default: { fail("Unsupported Ubuntu version! - $::operatingsystemrelease")  }
+          }
+        }
+        'Debian': { fail('Unsupported')  }
+        default: { fail('Unsupported Debian flavour!')  }
+      }
+    }
+    default: { fail('Unsupported OS!')  }
+  }
 }
