586b8b38cba2d09c88a4817ca1bbbe583ed08ed0
Jordi Prats <jordi.prats@gmail.com>
2018-05-17 16:18:26 +0200

    reordre ruby

--- lib/puppet/parser/functions/netmask2cidr.rb
+++ lib/puppet/parser/functions/netmask2cidr.rb
@@ -1,5 +1,11 @@
 require 'ipaddr'
 
+IPAddr.class_eval
+  def to_cidr
+    self.to_i.to_s(2).count("1")
+  end
+end
+
 Puppet::Parser::Functions::newfunction(:netmask2cidr, :type => :rvalue, :doc => <<-EOS
 Transform netmask to CIDR notation
 EOS
@@ -7,12 +13,6 @@ EOS
   raise(Puppet::ParseError, "netmask2cidr() wrong number of arguments. #{args.size} vs 1)") if args.size != 1
 
   arg = args[0]
-
-  IPAddr.class_eval
-    def to_cidr
-      self.to_i.to_s(2).count("1")
-    end
-  end
 
   IPAddr.new(arg).to_cidr
 
